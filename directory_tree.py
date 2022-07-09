48a5de61800d5c84d2a90d1a52c3215ff0e616bd
Molly <mollyrosebaxter@hotmail.co.uk>
2022-07-09 22:52:13 +0100

    directory tree code written to PEP8

--- directory_tree.py
+++ directory_tree.py
@@ -1,25 +1,23 @@
+"""Creation of directory tree"""
 from pathlib import Path
 
 # prefix components:
-space =  '    '
-branch = '│   '
+SPACE =  '    '
+BRANCH = '│   '
 # pointers:
-tee =    '├── '
-last =   '└── '
+TREE =    '├── '
+LAST =   '└── '
 
-
+""" function for directory tree"""
 def tree(dir_path: Path, prefix: str=''):
-    """A recursive generator, given a directory Path object
-    will yield a visual tree structure line by line
-    with each line prefixed by the same characters
-    """    
+    """directory tree"""
     contents = list(dir_path.iterdir())
     # contents each get pointers that are ├── with a final └── :
-    pointers = [tee] * (len(contents) - 1) + [last]
+    pointers = [TREE] * (len(contents) - 1) + [LAST]
     for pointer, path in zip(pointers, contents):
         yield prefix + pointer + path.name
-        if path.is_dir(): # extend the prefix and recurse:
-            extension = branch if pointer == tee else space 
+        if path.is_dir():
+            extension = BRANCH if pointer == TREE else SPACE
             # i.e. space because last, └── , above so no more |
             yield from tree(path, prefix=prefix+extension)
 
